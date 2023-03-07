85793b01b3e606cfd2588f2b020ee03e234113d7
hui <hui@findora.org>
2023-03-07 21:30:21 +0800

    limit block size up to 2 MB

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Change log
 
+#### v0.3.x
+
+- Limit block size up to 1 MB, in genesis
+
 #### v0.2.x
 
 - Make 'Seed Node' acting as a RPC server
--- src/tendermint_based/ddev/mod.rs
+++ src/tendermint_based/ddev/mod.rs
@@ -958,6 +958,8 @@ where
                             // when the chain is starting first time
                             g["genesis_time"].as_str().unwrap().replacen('2', "1", 1),
                         );
+                        g["consensus_params"]["block"]["max_bytes"] =
+                            serde_json::to_value((MB * 2).to_string()).unwrap();
                         self.meta.genesis = Some(serde_json::from_value(g).c(d!())?);
                         Ok(())
                     })
